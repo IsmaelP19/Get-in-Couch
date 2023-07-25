a95233b2f4816243b8e259abbc64526cf7f90d1b
IsmaelP19 <ismperort@alum.us.es>
2023-07-25 19:12:49 +0200

    feat(#020): create conversation on profile page

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { useAppContext } from '../context/state'
+import Link from 'next/link'
 export default function Message ({ message }) {
   const { user } = useAppContext()
   /*
@@ -18,12 +19,12 @@ export default function Message ({ message }) {
 
   return (
     <div className={`flex flex-col w-fit gap-2 p-2 rounded-2xl border-2 ${message.author.id === user.id ? 'border-blue-300 bg-blue-400 text-white self-end rounded-ee-none' : 'border-gray-200 bg-gray-300 rounded-es-none'}`}>
-      <div className='flex flex-row items-center justify-start gap-3 text-base '>
+
+      <Link href={`/profile/${message.author.username}`} className={`flex flex-row items-center justify-start gap-3 text-base hover:underline ${message.author.id === user.id ? 'hover:text-black' : 'hover:text-blue-600'}`}>
         <Image src={message.author.profilePhoto || '/static/images/default_avatar.png'} width={30} height={30} alt={message.author.username} />
-        <span>
-          {`${message.author.name} ${message.author.surname}`}
-        </span>
-      </div>
+        {`${message.author.name} ${message.author.surname}`}
+      </Link>
+
       <div className='flex flex-col items-start justify-center gap-3 text-base '>
         <span className=' text-base'>
           {message.message}
