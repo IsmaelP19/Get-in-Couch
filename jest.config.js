c934c1dd4bf060bc51c166a0ba4a1429af2c96a4
Ismael Pérez Ortiz <79575588+IsmaelP19@users.noreply.github.com>
2023-09-13 22:08:08 +0200

    fix: add collectCoverage options on jest config file

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,13 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-node'
+  testEnvironment: 'jest-environment-node',
+
+  // Add the collectCoverage and collectCoverageFrom options here:
+  collectCoverage: true, // Enable code coverage collection
+  collectCoverageFrom: [
+    '**/*.js'
+  ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
